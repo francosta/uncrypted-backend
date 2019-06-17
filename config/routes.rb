31d2943e69896ced5a76019d6911258ec979db2d
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-06-17 10:54:07 +0100

    Routes for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/users' => 'users#index'
   get '/users/:id' => 'users#show'
+  post '/users' => 'users#create'
+  patch '/users' => 'users#update'
+  delete '/users/:id' =>	'users#destroy'
   get '/currencies' => 'currencies#index'
   get '/currencies/:id' => 'currencies#show'
   post '/currencies' => 'currencies#create'
   patch '/currencies' => 'currencies#update'
   get '/markets' => 'markets#index'
   get '/markets/:id' => 'markets#show'
-<<<<<<< HEAD
   get '/portfolios' => 'portfolios#index'
-  get '/portfolios/:id' => 'portfolios#show'  
-=======
+  get '/portfolios/:id' => 'portfolios#show'
+  post '/portfolios' => 'portfolios#create'  
+  delete '/portfolios/:id' =>	'portfolios#destroy'
   get '/currency_portfolios' => 'currency_portfolios#index'
   get '/currency_portfolios/:id' => 'currency_portfolios#show'
->>>>>>> c1a47e13f20acdef7aa3f1ad0703765ca5835e7a
 end
