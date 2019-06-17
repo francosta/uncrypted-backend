774d0cd24d1ea6816e1e81dcbb28cc7bcd8f6760
github username <francisco@fcosta.pt>
2019-06-17 10:14:49 +0100

    Fized database

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,4 +1,4 @@
 class Market < ApplicationRecord
-    has_many :currency_markets
+    has_many :currency_markets, dependent: :destroy
     has_many :currencies, through: :currency_markets
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/users' => 'users#index'
-<<<<<<< HEAD
   get '/users/:id' => 'users#show'
   get '/currencies' => 'currencies#index'
   get '/currencies/:id' => 'currencies#show'
-=======
   get '/users/:id' => 'users#show' 
   get '/markets' => 'markets#index'
   get '/markets/:id' => 'markets#show' 
->>>>>>> 785160692d996dd0484ffab7f4b20ea0aa1c43c0
 end
