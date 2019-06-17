edf113fe163b71ed74f10c1db4e67812e9b132aa
github username <francisco@fcosta.pt>
2019-06-17 09:42:20 +0100

    Created currency controller.

---
+++ app/controllers/currencies_controller.rb
@@ -0,0 +1,37 @@
+class CurrenciesController < ApplicationController
+    def index
+        currencies = Currency.all
+        render json: currencies
+    end
+
+    def show
+        currency = Currency.all.find_by(params[:id])
+        render json: currency
+    end
+
+    def create
+        currency = Currency.new(ticker: currency_params[:ticker], target: currency_params[:target], price: currency_params[:price], volume: currency_params[:volume], change: currency_params[:change])
+        
+        if currency.save
+            render json: currency
+        else
+           render json: {
+             error: "The currency was not created. Please try again later.",
+             status: 400
+             }, status: 400
+        end
+    end
+
+    def update
+        currency = Currency.find_by(id: params[:id])
+        currency.update(currency_params)
+        render json: currency
+    end
+
+    private
+
+    def currencies_params
+        params.require(:currencies).permit(:id, :ticker, :target, :price, :volume, :change)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/users' => 'users#index'
-  get '/users/:id' => 'users#show' 
+  get '/users/:id' => 'users#show'
+  get '/currencies' => 'currencies#index'
+  get '/currencies/:id' => 'currencies#show'
 end
